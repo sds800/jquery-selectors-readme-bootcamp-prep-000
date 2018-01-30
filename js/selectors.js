2df3e0c6e9d2ed8230684fe31a837d34b2dbc19a
SDS 800 <jeremyhyman@gmail.com>
2018-01-30 20:15:45 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,10 +1,15 @@
 // declare your functions here...
 function paragraphSelector(){
 
-return $('p')
+  return $('p')
 }
 
 function lastImageSelector(){
-return  $('img:last')
 
+  return  $('img:last')
+
+}
+function ninjaBabySelector() {
+
+  return $('#baby-ninja')
 }
